3a2da1fbe7a87b4bcae2e605b8f987bd29c442a5
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-02-03 20:16:15 +0300

    add login and registration view controller

---
+++ Insta-Planner/Controllers/Other/Onboarding/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Insta-Planner
+//
+//  Created by Ekaterina Volkova on 03/02/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Insta-Planner/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationViewController.swift
+//  Insta-Planner
+//
+//  Created by Ekaterina Volkova on 03/02/2022.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
