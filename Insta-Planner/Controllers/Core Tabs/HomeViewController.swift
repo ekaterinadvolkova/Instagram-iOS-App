d5be57fcb23c6480fd18c186cd107c686815eb61
Ekaterina Volkova <ekaterinad.volkova@gmail.com>
2022-02-07 00:38:17 +0300

    Add redirection to login if the user is not authenticated

--- Insta-Planner/Controllers/Core Tabs/HomeViewController.swift
+++ Insta-Planner/Controllers/Core Tabs/HomeViewController.swift
@@ -1,17 +1,26 @@
-//
-//  ViewController.swift
-//  Insta-Planner
-//
-//  Created by Ekaterina Volkova on 03/02/2022.
-//
-
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handleNotAuthenticated()
+        
+    }
+    
+    private func handleNotAuthenticated(){
+        // Check authentication status
+        if Auth.auth().currentUser == nil {
+            //show login
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: false)
+        }
     }
 
 
