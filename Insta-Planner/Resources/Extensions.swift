3c5aa603f1c48d806e8fb633e4d7aa71fc38b2c2
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-02-16 15:33:45 +0300

    Add more extensions to the extensions file

--- Insta-Planner/Resources/Extensions.swift
+++ Insta-Planner/Resources/Extensions.swift
@@ -9,7 +9,36 @@ import UIKit
 
 extension UIView {
     
+    //width
     public var width: CGFloat{
         return frame.size.width
     }
+    
+    //height
+    public var height: CGFloat{
+        return frame.size.height
+    }
+    
+    //top
+    public var top: CGFloat{
+        return frame.origin.y
+    }
+    
+    //bottom
+    public var bottom: CGFloat{
+        return frame.origin.y + frame.size.height
+    }
+    
+    //left
+    public var left: CGFloat{
+        return frame.origin.x
+    }
+    
+    //right
+    public var right: CGFloat{
+        return frame.origin.x + frame.size.width
+    }
+    
+    
+     
 }
