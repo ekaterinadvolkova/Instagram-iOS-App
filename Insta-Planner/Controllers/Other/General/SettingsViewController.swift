bc56ee4a0ea2f6212464b550eaa304c65adc46c8
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-02-24 01:55:37 +0300

    Settings View Controller:  add TableView, frames

--- Insta-Planner/Controllers/Other/General/SettingsViewController.swift
+++ Insta-Planner/Controllers/Other/General/SettingsViewController.swift
@@ -7,12 +7,58 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct SettingCellModel {
+    let title: String
+    let handler: (()-> Void)
+    
+}
+
+/// View Controller to show user settings
+final class SettingsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
+        return tableView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    //set frame
+    override func viewDidLayoutSubviews() {
+        tableView.frame = view.bounds
+        
     }
 
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Handle cell selection
+    }
+    
 }
